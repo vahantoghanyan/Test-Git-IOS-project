9fc967d47bc65dd23e0f671195f3b139ca5b6be3
v.toganyan <v.toganyan@mac514rih.rambler.ramblermedia.com>
2017-06-15 16:33:52 +0300

    added a Test Class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by v.toganyan on 15.06.17.
+//  Copyright © 2017 v.toganyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by v.toganyan on 15.06.17.
+//  Copyright © 2017 v.toganyan. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
